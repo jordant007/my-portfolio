bb69ad46964d52704536e5999dc86ca82229da88
Jordant007 <jordantyrill@gmail.com>
2025-01-17 08:40:33 +0300

    added key on environment

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    RESEND_API_KEY: process.env.RESEND_API_KEY, // Add this line to expose the environment variable
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
